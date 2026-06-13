3038ab90f2060d382e11a2c2294e677710bb7ec9
sarkenaes <sarkenaes@gmail.com>
2026-06-13 08:21:19 -0500

    Finished database

--- db.c
+++ db.c
@@ -7,3 +7,17 @@ int db_init(){
 sqlite3_close(db);
 return 0;
 }
+int db_store(const char *filepath,const  char *hash ){
+sqlite3 *db;
+sqlite3-smt *stmt; 
+if(sqlite3_open("fim.db",&db)!=SQLITE_OK){
+    return -1;
+}
+   sqlite3_prepare_v2(db,"INSERT OR REPLACE INTO files (filepath,hash) VALUES (? ,?);", -1, &stmt,NULL);
+   sqlite3_bind_text(stmt,1,filepath,-1,SQLITE_STATIC);
+   sqlite3_bind_text(stmt,2,hash,-1,SQLITE_STATIC);
+   sqlite3_step(stmt);
+   sqlite3_finalize(stmt);
+sqlite3_close(db);
+return 0;
+}
