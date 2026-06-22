801aec19bcb612ac0101f4266d8317f8514280c5
sarkenaes <sarkenaes@gmail.com>
2026-06-22 07:49:32 -0500

    Added CLI flags

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 fim: main.c hasher.c db.c watcher.c
-	gcc main.c hasher.c db.c watcher.c -lssl -lcrypto -lsqlite3 -0 fim
+	gcc main.c hasher.c db.c watcher.c -lssl -lcrypto -lsqlite3 -o fim
--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 #include<stdio.h>
 #include<string.h>
 #include"watcher.h"
-int main(){
+int main(int argc, char *argv[]){
+    if(argc<3){
+        printf("please enter a valid input");
+        return 1;
+    }
 char out_hash[65];
 char out_hex[65];
 db_init();
-hash_file("test.txt", out_hex, sizeof(out_hex));
-db_store("test.txt", out_hex);
-printf("Baseline stored for test.txt: %s\n", out_hex);
-watch_file("test.txt");
+if(strcmp(argv[1],"--baseline")==0){
+hash_file(argv[2], out_hex, sizeof(out_hex));
+db_store(argv[2], out_hex);
+printf("Baseline stored for %s : %s\n",argv[2], out_hex);}
+if(strcmp(argv[1],"--watch")==0)
+    watch_file(argv[2]);
 return 0;
 }
 
